9923c638bb38c73e039c0c82ec15ebbb00d65184
Remy102 <remys0450@gmail.com>
2023-08-01 03:26:32 +0300

    feat: create cart page

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-details.component.scss']
 })
 export class CartDetailsComponent {
+  isLoading: boolean = false;
+  origin: string = '';
+  cartItems: any[] = [];
 
+  constructor(private router: Router, private route: ActivatedRoute, private cartService: CartService) {
+    this.cartItems = this.cartService.getCartItems();
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.origin = params['origin'];
+    });
+  }
+
+  backList() {
+    this.router.navigateByUrl(this.origin);
+  }
+
+  backHome() {
+    this.router.navigate(['/']);
+  }
+
+  redirectToDetailPage(isLoading: boolean, product: any) {
+    if(isLoading) return;
+    this.router.navigate(['/detail', product.url, this.router.url]);
+  }
+
+  deleteFromCart(product: any) {
+    this.cartService.deleteProduct(product);
+    this.cartItems = this.cartService.getCartItems();
+  }
+
+  handleImageError(product: any) {
+    product['imageUrl'] = 'assets/images/no_image.png';
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class CartService {
 
   constructor() { }
+
+  private cartItems: any[] = [];
+
+  addToCart(product: any, quentity: number) {
+    for (let i = 0; i < this.cartItems.length; i++) {
+      if (this.cartItems[i].url == product.url) {
+        this.cartItems[i]['quentity'] = this.cartItems[i]['quentity'] + quentity;
+
+        return;
+      }
+    }
+    product['quentity'] = quentity;
+    this.cartItems.push(product);
+  }
+
+  deleteProduct(product: any) {
+    this.cartItems = this.cartItems.filter((item) => item.url != product.url);
+  }
+
+  getCartItems(): any[] {
+    return this.cartItems;
+  }
 }
